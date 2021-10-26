43621efa3efda1bd501d2a40be860a3cf09dd52a
Gareth Rogers <garethdavisrogers@gmail.com>
2021-10-26 15:47:49 -0400

    socket functional from client to server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+const socket = io("http://localhost:3000");
 
-class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {};
-  }
-
-  render() {
-    return <div>Hello World</div>;
-  }
-}
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const submitText = () => {
+    console.log(inputText);
+  };
+  return (
+    <div>
+      <h1>Chat MicroService</h1>
+      <div>Chats go here</div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      />
+      <button onClick={submitText}>Send</button>
+    </div>
+  );
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
+const http = require("http");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 app.use(express.static("./client/public"));
 
-app.get('/', (req, res)=>{
-  res.send('hey')
-})
-app.listen(port, () => {
+io.on("connection", (socket) => {
+  console.log("connected to socket");
+  socket.on("disconnect", () => {
+    console.log("user has left");
+  });
+});
+
+server.listen(port, () => {
   console.log("node is listening");
 });
